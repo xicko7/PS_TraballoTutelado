862188a8da5a33c1941068cc32bbf21b4177ad4e
Xico Fernández <xico.fernandez@udc.es>
2021-12-31 17:47:45 +0100

    Cambiado estética do dicionario e enlazado co botón do menú

--- App/app/src/main/java/com/example/forcapp/Dictionary.java
+++ App/app/src/main/java/com/example/forcapp/Dictionary.java
@@ -20,29 +20,45 @@ import java.util.ArrayList;
 public class Dictionary extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    Button add;
+    Button add, back;
     private WordsAdapter mAdapter;
     int added;
+    ArrayList<Word> initialData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dictionary_layout);
-
-        recyclerView = findViewById(R.id.word_rv);
-        add = findViewById(R.id.button_addWord);
-
-        ArrayList<Word> initialData = new ArrayList<>();
+        getSupportActionBar().hide();
 
 //        Engadir primeiros elementos
         for (int i = 0; i < 10; i++)
             initialData.add(new Word(Integer.toString(i)));
 
-        mAdapter = new WordsAdapter (initialData);
+        setUI();
+
+    }
+
+    private void setUI() {
+
+
+        recyclerView = findViewById(R.id.word_rv);
+        add = findViewById(R.id.button_addWord);
+        back = findViewById(R.id.dictionary_back_button);
+
+        mAdapter = new WordsAdapter(initialData);
         LinearLayoutManager linearLayoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(mAdapter);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent menuIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(menuIntent);
+            }
+        });
 
         mAdapter.setClickListener(new WordsAdapter.OnItemClickListener() {
             @Override
@@ -60,7 +76,7 @@ public class Dictionary extends AppCompatActivity {
 
     }
 
-    private void createAddDialog(WordsAdapter mAdapter){
+    private void createAddDialog(WordsAdapter mAdapter) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.add_dialog);
 
@@ -73,22 +89,22 @@ public class Dictionary extends AppCompatActivity {
         builder.setPositiveButton(R.string.dialogOK, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(),R.string.dialogOK, Toast.LENGTH_SHORT).show();
-                added =  Integer.parseInt(input.getText().toString());
+                Toast.makeText(getApplicationContext(), R.string.dialogOK, Toast.LENGTH_SHORT).show();
+                added = Integer.parseInt(input.getText().toString());
                 mAdapter.addWord(String.valueOf(input.getText()));
             }
         });
         builder.setNegativeButton(R.string.dialogCancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(),R.string.dialogCancel, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.dialogCancel, Toast.LENGTH_SHORT).show();
             }
         });
         AlertDialog alert = builder.create();
         alert.show();
     }
 
-    private void createDeleteDialog(WordsAdapter mAdapter, int position){
+    private void createDeleteDialog(WordsAdapter mAdapter, int position) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.remove_dialog);
 
--- App/app/src/main/java/com/example/forcapp/MainActivity.java
+++ App/app/src/main/java/com/example/forcapp/MainActivity.java
@@ -2,14 +2,36 @@ package com.example.forcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button dictionaryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        setUI();
+
+    }
+
+    private void setUI() {
+        dictionaryButton = findViewById(R.id.diccionario_bt);
+
+        dictionaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dictionaryIntent = new Intent(getApplicationContext(), Dictionary.class);
+                startActivity(dictionaryIntent);
+            }
+        });
+
     }
 }
