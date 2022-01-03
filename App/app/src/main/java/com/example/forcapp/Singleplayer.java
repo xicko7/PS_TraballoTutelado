a94a1838977c449953a8d3e9b894fa5007d6930f
Xico Fernández <xico.fernandez@udc.es>
2022-01-03 05:09:25 +0100

    Engadido layout elemento a elemento

--- App/app/src/main/java/com/example/forcapp/Singleplayer.java
+++ App/app/src/main/java/com/example/forcapp/Singleplayer.java
@@ -1,9 +1,14 @@
 package com.example.forcapp;
 
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class Singleplayer extends AppCompatActivity {
 
@@ -18,5 +23,30 @@ public class Singleplayer extends AppCompatActivity {
 
         adapter = new LetterAdapter(getApplicationContext());
         gridView.setAdapter(adapter);
+
+        setUI();
+    }
+
+    private void setUI() {
+
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+
+        ids.add(R.id.fault1);
+        ids.add(R.id.fault2);
+        ids.add(R.id.fault3);
+        ids.add(R.id.fault4);
+        ids.add(R.id.fault5);
+        ids.add(R.id.fault6);
+        ids.add(R.id.fault7);
+        ids.add(R.id.fault8);
+        ids.add(R.id.fault9);
+        ids.add(R.id.fault10);
+        ids.add(R.id.fault11);
+
+        for (int i = 0; i < ids.size(); i++) {
+
+            ImageView imageViewIcon = findViewById(ids.get(i));
+            imageViewIcon.setColorFilter(getApplicationContext().getResources().getColor(R.color.white));
+        }
     }
 }
