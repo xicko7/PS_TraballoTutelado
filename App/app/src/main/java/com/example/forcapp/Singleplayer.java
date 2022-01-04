bdda172fe40c012ce3dc40762bbd8726b43c7cc3
Xico Fernández <xico.fernandez@udc.es>
2022-01-04 14:37:51 +0100

    Cambiado o LetterAdapter de sitio

--- App/app/src/main/java/com/example/forcapp/Singleplayer.java
+++ App/app/src/main/java/com/example/forcapp/Singleplayer.java
@@ -1,14 +1,21 @@
 package com.example.forcapp;
 
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.forcapp.database.WordDatabaseClient;
+import com.example.forcapp.entity.Word;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +24,19 @@ public class Singleplayer extends AppCompatActivity {
 
     private LetterAdapter adapter;
     private GridView gridView;
-    static List<ImageView> faults = new ArrayList();
-    static int intentos = 0;
+    private List<ImageView> faults = new ArrayList();
+    private int intentos = 0;
+    private String randomWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_game);
         gridView = findViewById(R.id.letters);
-
         adapter = new LetterAdapter(getApplicationContext());
         gridView.setAdapter(adapter);
+        getRandomWord();
 
         setUI();
     }
@@ -57,13 +66,89 @@ public class Singleplayer extends AppCompatActivity {
         }
     }
 
-    static void tapLetter(String letter, View view) {
-        Toast.makeText(view.getContext(), "Pulsada letra " + letter, Toast.LENGTH_SHORT).show();
-        view.setEnabled(false);
-        view.setVisibility(View.GONE);
+    void getRandomWord() {
+        class GetRandomWord extends AsyncTask<Void, Void, List<Word>> { // claseinterna
+            @Override
+            protected List<Word> doInBackground(Void... voids) {
+                List<Word> words = WordDatabaseClient.getInstance(getApplicationContext()).getWordDatabase().getWordDao().getAllWords();
+                return words;
+            }
 
-        faults.get(intentos).setVisibility(View.VISIBLE);
-        intentos++;
+            @Override
+            protected void onPostExecute(List<Word> words) {
+                super.onPostExecute(words); // Actualizar la UI
+                randomWord = words.get(getRandomNumber(0, words.size() - 1)).getWord();
+                Toast.makeText(getApplicationContext(), randomWord, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        GetRandomWord gf = new GetRandomWord(); // Crear una instancia y ejecutar
+        gf.execute();
 
     }
+
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    private class LetterAdapter extends BaseAdapter {
+        private String[] letters;
+        private LayoutInflater letterInf;
+        private Context context;
+
+        public LetterAdapter(Context context) {
+            letters = new String[26];
+            this.context = context;
+            for (int i = 0; i < letters.length; i++) {
+                letters[i] = "" + (char) (i + 'A');
+            }
+            letterInf = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return letters.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Button buttonLetter;
+            if (view == null) {
+                buttonLetter = (Button) letterInf.inflate(R.layout.letters, viewGroup, false);
+            } else {
+                buttonLetter = (Button) view;
+            }
+            buttonLetter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tapLetter(letters[i], view);
+                }
+            });
+            buttonLetter.setText(letters[i]);
+            return buttonLetter;
+        }
+
+        void tapLetter(String letter, View view) {
+            Toast.makeText(getApplicationContext(), "Pulsada letra " + letter, Toast.LENGTH_SHORT).show();
+            view.setEnabled(false);
+            view.setVisibility(View.GONE);
+
+            if (intentos < 11) {
+                faults.get(intentos).setVisibility(View.VISIBLE);
+                intentos++;
+            }
+        }
+
+    }
+
 }
