4014679c1d00dbc6fdc79eb7c299a0382143c1c4
Xico Fernández <xico.fernandez@udc.es>
2022-01-04 11:53:08 +0100

    Listener para cada letra

--- App/app/src/main/java/com/example/forcapp/LetterAdapter.java
+++ App/app/src/main/java/com/example/forcapp/LetterAdapter.java
@@ -1,20 +1,24 @@
 package com.example.forcapp;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LetterAdapter extends BaseAdapter {
-    private String [] letters;
+    private String[] letters;
     private LayoutInflater letterInf;
+    private Context context;
 
-    public LetterAdapter (Context context){
+    public LetterAdapter(Context context) {
         letters = new String[26];
-        for (int i = 0; i<letters.length; i++){
-            letters[i]=""+(char)(i+'A');
+        this.context = context;
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = "" + (char) (i + 'A');
         }
         letterInf = LayoutInflater.from(context);
     }
@@ -37,13 +41,23 @@ public class LetterAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         Button buttonLetter;
-        if (view == null){
+        if (view == null) {
             buttonLetter = (Button) letterInf.inflate(R.layout.letters, viewGroup, false);
+        } else {
+            buttonLetter = (Button) view;
         }
-        else{
-            buttonLetter=(Button) view;
-        }
+        buttonLetter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tapLetter(letters[i]);
+            }
+        });
         buttonLetter.setText(letters[i]);
         return buttonLetter;
+    }
+
+    void tapLetter(String letter) {
+        // Función executada ao pulsar o botón
+        Toast.makeText(context, "Pulsada letra " + letter, Toast.LENGTH_SHORT).show();
     }
 }
