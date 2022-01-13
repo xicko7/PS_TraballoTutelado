abd6d47bc413effa73b5f86131fdbd63da612184
MarioPaez <mariopaez_99@hotmail.com>
2022-01-13 19:30:38 +0100

    errorToast usado con resurcos

--- App/app/src/main/java/com/example/forcapp/ResetPassword.java
+++ App/app/src/main/java/com/example/forcapp/ResetPassword.java
@@ -74,63 +74,63 @@ public class ResetPassword extends AppCompatActivity {
              */
 
             case "ERROR_INVALID_CUSTOM_TOKEN":
-                Toast.makeText(ResetPassword.this, "El formato del token personalizado es incorrecto. Por favor revise la documentación", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_INVALID_CUSTOM_TOKEN, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                Toast.makeText(ResetPassword.this, "El token personalizado corresponde a una audiencia diferente.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_CUSTOM_TOKEN_MISMATCH, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_INVALID_CREDENTIAL":
-                Toast.makeText(ResetPassword.this, "La credencial de autenticación proporcionada tiene un formato incorrecto o ha caducado.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_INVALID_CREDENTIAL, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_INVALID_EMAIL":
-                Toast.makeText(ResetPassword.this, "La dirección de correo electrónico está mal formateada.", Toast.LENGTH_LONG).show();
-                emailET.setError("La dirección de correo electrónico está mal formateada.");
+                Toast.makeText(ResetPassword.this, R.string.ERROR_INVALID_EMAIL, Toast.LENGTH_LONG).show();
+                emailET.setError(String.valueOf(R.string.ERROR_INVALID_EMAIL));
                 emailET.requestFocus();
                 break;
 
             case "ERROR_USER_MISMATCH":
-                Toast.makeText(ResetPassword.this, "Las credenciales proporcionadas no corresponden al usuario que inició sesión anteriormente..", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_USER_MISMATCH, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_REQUIRES_RECENT_LOGIN":
-                Toast.makeText(ResetPassword.this, "Esta operación es sensible y requiere autenticación reciente. Inicie sesión nuevamente antes de volver a intentar esta solicitud.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_REQUIRES_RECENT_LOGIN, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                Toast.makeText(ResetPassword.this, "Ya existe una cuenta con la misma dirección de correo electrónico pero diferentes credenciales de inicio de sesión. Inicie sesión con un proveedor asociado a esta dirección de correo electrónico.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_EMAIL_ALREADY_IN_USE":
-                Toast.makeText(ResetPassword.this, "La dirección de correo electrónico ya está siendo utilizada por otra cuenta..   ", Toast.LENGTH_LONG).show();
-                emailET.setError("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
+                Toast.makeText(ResetPassword.this, R.string.ERROR_EMAIL_ALREADY_IN_USE, Toast.LENGTH_LONG).show();
+                emailET.setError(String.valueOf(R.string.ERROR_EMAIL_ALREADY_IN_USE));
                 emailET.requestFocus();
                 break;
 
             case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                Toast.makeText(ResetPassword.this, "Esta credencial ya está asociada con una cuenta de usuario diferente.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_CREDENTIAL_ALREADY_IN_USE, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_USER_DISABLED":
-                Toast.makeText(ResetPassword.this, "La cuenta de usuario ha sido inhabilitada por un administrador..", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_USER_DISABLED, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_USER_TOKEN_EXPIRED":
-                Toast.makeText(ResetPassword.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_USER_TOKEN_EXPIRED, Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_USER_NOT_FOUND":
-                Toast.makeText(ResetPassword.this, "No hay ningún registro de usuario que corresponda a este identificador. Es posible que se haya eliminado al usuario.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this,R.string.ERROR_USER_NOT_FOUND , Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_INVALID_USER_TOKEN":
-                Toast.makeText(ResetPassword.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this,R.string.ERROR_INVALID_USER_TOKEN , Toast.LENGTH_LONG).show();
                 break;
 
             case "ERROR_OPERATION_NOT_ALLOWED":
-                Toast.makeText(ResetPassword.this, "Esta operación no está permitida. Debes habilitar este servicio en la consola.", Toast.LENGTH_LONG).show();
+                Toast.makeText(ResetPassword.this, R.string.ERROR_OPERATION_NOT_ALLOWED, Toast.LENGTH_LONG).show();
                 break;
 
         }
